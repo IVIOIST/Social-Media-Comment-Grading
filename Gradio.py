29d2aada96e9932788144753d63804d18b0630e9
IVIOIST <higgsboson16@protonmail.com>
2022-07-05 23:41:20 +1000

    Made gradio implementation object based

--- Gradio.py
+++ Gradio.py
@@ -5,9 +5,7 @@ import tensorflow as tf
 import pandas as pd
 import os
 from tensorflow.keras.models import load_model
-from tensorflow.keras.layers import TextVectorization
 
-labels_list = ['Toxic', 'Severe toxic', 'Obscene', 'Threat', 'Insult', 'Identity hate']
 
 class Toxicity:
     def __init__(self):
@@ -24,15 +22,20 @@ class Toxicity:
 
 toxicity = Toxicity()
 
+labels_list = ['Toxic', 'Severe toxic', 'Obscene', 'Threat', 'Insult', 'Identity hate']
+
 def score_comment(comment):
+    comment = comment
     vectorized_comment = toxicity.vect_str([comment])
     prediction = toxicity.predict(vectorized_comment)
-
     text = ''
+    plt.bar(labels_list, prediction[0], color='teal', width=0.4)
     for idx, col in enumerate(labels_list):
         text += f'{col}:{round(prediction[0][idx]*100, 2)}%\n'
-    return text
+    
+    return text, plt.gcf()
 
-interface = gr.Interface(fn=score_comment, inputs=gr.inputs.Textbox(lines=2, placeholder='Comment to score'),outputs='text')
 
-interface.launch(share=True)
+interface = gr.Interface(fn=score_comment, inputs=gr.inputs.Textbox(lines=2, placeholder='Comment to score'),outputs=['text', 'plot'])
+
+interface.launch(share=False)
