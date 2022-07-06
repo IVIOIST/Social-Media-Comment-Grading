ac3c1a0d5b8d444a510ecbebd38e90d82bc684b5
IVIOIST <higgsboson16@protonmail.com>
2022-07-06 23:01:51 +1000

    Got visualisation working finally

--- Gradio.py
+++ Gradio.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import matplotlib.pyplot as plt
 import gradio as gr
 import numpy as np 
@@ -29,13 +30,15 @@ def score_comment(comment):
     vectorized_comment = toxicity.vect_str([comment])
     prediction = toxicity.predict(vectorized_comment)
     text = ''
-    plt.bar(labels_list, prediction[0], color='teal', width=0.4)
+    prediction_list = []
     for idx, col in enumerate(labels_list):
         text += f'{col}:{round(prediction[0][idx]*100, 2)}%\n'
-    
-    return text, plt.gcf()
+        prediction_list.append(float(prediction[0][idx]))
+    dictionary = (dict(zip(labels_list, prediction_list)))   
+    print(type(dictionary)) 
+    return dictionary
+  
+outputs = [gr.Label(num_top_classes=6, label='Breakdown')]
 
-
-interface = gr.Interface(fn=score_comment, inputs=gr.inputs.Textbox(lines=2, placeholder='Comment to score'),outputs=['text', 'plot'])
-
-interface.launch(share=False)
+if __name__ == '__main__':
+    gr.Interface(fn=score_comment, inputs=gr.inputs.Textbox(lines=2, placeholder='Comment to score'),outputs=outputs).launch()
